82a9257aa81c431a94c25c9dff1aa907db192323
soham-rc <sohamroy425@gmail.com>
2025-12-13 00:31:21 +0530

    finished bug fixes

--- src/models/Admin.js
+++ src/models/Admin.js
@@ -31,19 +31,13 @@ const adminSchema = new mongoose.Schema(
     }
 );
 
-adminSchema.pre('save', async function (next) {
+adminSchema.pre('save', async function () {
   if (!this.isModified('password')) {
-    return next();
+    return;
   }
 
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } 
-  catch (error) {
-    next(error);
-  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 adminSchema.methods.comparePassword = async function (candidatePassword) {
